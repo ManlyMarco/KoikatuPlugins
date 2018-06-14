3909050f158120fb08a08906831617a5d9f2b1b5
Keelhauled <keelhauledhs@gmail.com>
2018-06-14 04:31:41 +0300

    BetterSceneLoader

---
+++ BetterSceneLoader/BetterSceneLoaderInit.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace BetterSceneLoader
+{
+    [BepInProcess("CharaStudio")]
+    [BepInPlugin("com.keelhauled.bettersceneloader", "BetterSceneLoader", "1.1.1")]
+    class BetterSceneLoaderInit : BaseUnityPlugin
+    {
+        void OnLevelWasLoaded(int level)
+        {
+            if(SceneManager.GetActiveScene().name == "Studio") gameObject.AddComponent<BetterSceneLoader>();
+        }
+    }
+}

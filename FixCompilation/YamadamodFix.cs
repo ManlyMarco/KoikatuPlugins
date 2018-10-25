d6a808473dd619e0f7ecd100a83ec654900452be
Keelhauled <keelhauledhs@gmail.com>
2018-10-25 18:49:28 +0300

    FixCompilation - Remove broken patches

--- FixCompilation/YamadamodFix.cs
+++ FixCompilation/YamadamodFix.cs
@@ -32,52 +32,52 @@ namespace FixCompilation
             return false;
         }
 
-        [HarmonyPrefix, HarmonyPatch(typeof(GuideObject), "LateUpdate")]
-        public static bool HarmonyPatch_GuideObject_LateUpdate(GuideObject __instance)
-        {
-            if(__instance.transformTarget) return true;
+        //[HarmonyPrefix, HarmonyPatch(typeof(GuideObject), "LateUpdate")]
+        //public static bool HarmonyPatch_GuideObject_LateUpdate(GuideObject __instance)
+        //{
+        //    if(__instance.transformTarget) return true;
 
-            var traverse = Traverse.Create(__instance);
-            var roots = traverse.Field("roots").GetValue<GameObject[]>();
+        //    var traverse = Traverse.Create(__instance);
+        //    var roots = traverse.Field("roots").GetValue<GameObject[]>();
 
-            if(__instance.parent && __instance.nonconnect)
-            {
-                traverse.Method("CalcPosition").GetValue();
-                traverse.Method("CalcRotation").GetValue();
-            }
+        //    if(__instance.parent && __instance.nonconnect)
+        //    {
+        //        traverse.Method("CalcPosition").GetValue();
+        //        traverse.Method("CalcRotation").GetValue();
+        //    }
 
-            var mode = __instance.mode;
-            if(mode != GuideObject.Mode.Local)
-            {
-                if(mode == GuideObject.Mode.World)
-                    roots[0].transform.eulerAngles = Vector3.zero;
-            }
-            else
-            {
-                roots[0].transform.eulerAngles = ((!__instance.parent) ? Vector3.zero : __instance.parent.eulerAngles);
-            }
+        //    var mode = __instance.mode;
+        //    if(mode != GuideObject.Mode.Local)
+        //    {
+        //        if(mode == GuideObject.Mode.World)
+        //            roots[0].transform.eulerAngles = Vector3.zero;
+        //    }
+        //    else
+        //    {
+        //        roots[0].transform.eulerAngles = ((!__instance.parent) ? Vector3.zero : __instance.parent.eulerAngles);
+        //    }
 
-            return false;
-        }
+        //    return false;
+        //}
 
-        [HarmonyPrefix, HarmonyPatch(typeof(FKCtrl), "LateUpdate")]
-        public static bool HarmonyPatch_FKCtrl_LateUpdate(FKCtrl __instance)
-        {
-            var traverse = Traverse.Create(__instance);
+        //[HarmonyPrefix, HarmonyPatch(typeof(FKCtrl), "LateUpdate")]
+        //public static bool HarmonyPatch_FKCtrl_LateUpdate(FKCtrl __instance)
+        //{
+        //    var traverse = Traverse.Create(__instance);
 
-            int count = traverse.Property("count").GetValue<int>();
-            for(int i = 0; i < count; i++)
-            {
-                var listBones = traverse.Field("listBones").GetValue<IList>();
-                if(listBones[i] != null)
-                {
-                    var bone = Traverse.Create(listBones[i]);
-                    if(bone.Property("gameObject").GetValue<GameObject>())
-                        bone.Method("Update").GetValue();
-                }
-            }
+        //    int count = traverse.Property("count").GetValue<int>();
+        //    for(int i = 0; i < count; i++)
+        //    {
+        //        var listBones = traverse.Field("listBones").GetValue<IList>();
+        //        if(listBones[i] != null)
+        //        {
+        //            var bone = Traverse.Create(listBones[i]);
+        //            if(bone.Property("gameObject").GetValue<GameObject>())
+        //                bone.Method("Update").GetValue();
+        //        }
+        //    }
 
-            return false;
-        }
+        //    return false;
+        //}
     }
 }

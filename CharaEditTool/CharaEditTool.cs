484bdf47e09b8024a2d52eae718bac1a4ce246e6
Keelhauled <keelhauledhs@gmail.com>
2018-10-31 02:17:42 +0200

    CharaEditTool - MoreAccessories support

--- CharaEditTool/CharaEditTool.cs
+++ CharaEditTool/CharaEditTool.cs
@@ -1,13 +1,14 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel;
-using BepInEx;
 using UnityEngine;
-using Harmony;
-using ChaCustom;
+using UnityEngine.SceneManagement;
+using BepInEx;
 using static BepInEx.Logger;
 using BepInEx.Logging;
-using UnityEngine.SceneManagement;
-using System.Collections;
+using Harmony;
+using ChaCustom;
 
 namespace CharaEditTool
 {
@@ -16,6 +17,10 @@ namespace CharaEditTool
     {
         Color? savedColor;
         bool inScene = false;
+        object moreAccObj;
+        CustomAcsChangeSlot accMenu;
+        CvsAccessory[] cvsAccessory;
+        ChaControl chara;
 
         [Description("Press hotkey to save the selected color.\n" +
                      "Hold hotkey to apply the saved color to every accessory color slot that has the same color as the selected color.")]
@@ -54,7 +59,11 @@ namespace CharaEditTool
                 if(!inScene)
                 {
                     inScene = true;
-                    StartCoroutine(InputCheck());
+                    moreAccObj = GameObject.Find("BepInEx_Manager").GetComponent("MoreAccessoriesKOI.MoreAccessories");
+                    accMenu = FindObjectOfType<CustomAcsChangeSlot>();
+                    cvsAccessory = Traverse.Create(accMenu).Field("cvsAccessory").GetValue<CvsAccessory[]>();
+                    chara = CustomBase.Instance.chaCtrl;
+                    //StartCoroutine(InputCheck());
                 }
             }
             else
@@ -63,15 +72,24 @@ namespace CharaEditTool
             }
         }
 
-        IEnumerator InputCheck()
+        void Update() // for ScriptEngine
         {
-            while(inScene)
+            if(inScene)
             {
                 colorCopyHotkey.KeyHoldAction(SetColor);
                 colorCopyHotkey.KeyUpAction(SaveColor);
-                yield return null;
             }
         }
+
+        //IEnumerator InputCheck()
+        //{
+        //    while(inScene)
+        //    {
+        //        colorCopyHotkey.KeyHoldAction(SetColor);
+        //        colorCopyHotkey.KeyUpAction(SaveColor);
+        //        yield return null;
+        //    }
+        //}
 
         void SaveColor()
         {
@@ -86,52 +104,86 @@ namespace CharaEditTool
         {
             if(savedColor != null)
             {
-                var chara = CustomBase.Instance.chaCtrl;
-                var accMenu = FindObjectOfType<CustomAcsChangeSlot>();
-                var cvsAccessory = Traverse.Create(accMenu).Field("cvsAccessory").GetValue<CvsAccessory[]>();
                 var selectedColor = GetSelectedColor();
 
-                foreach(var acc in cvsAccessory)
+                if(selectedColor != null)
                 {
-                    var color0 = chara.nowCoordinate.accessory.parts[(int)acc.slotNo].color[0];
-                    if(color0 == selectedColor) acc.UpdateAcsColor01(savedColor.Value);
+                    foreach(var acc in cvsAccessory)
+                    {
+                        int slotNo = (int)acc.slotNo;
 
-                    var color1 = chara.nowCoordinate.accessory.parts[(int)acc.slotNo].color[1];
-                    if(color1 == selectedColor) acc.UpdateAcsColor02(savedColor.Value);
+                        var color0 = chara.nowCoordinate.accessory.parts[slotNo].color[0];
+                        if(color0 == selectedColor) acc.UpdateAcsColor01(savedColor.Value);
 
-                    var color2 = chara.nowCoordinate.accessory.parts[(int)acc.slotNo].color[2];
-                    if(color2 == selectedColor) acc.UpdateAcsColor03(savedColor.Value);
+                        var color1 = chara.nowCoordinate.accessory.parts[slotNo].color[1];
+                        if(color1 == selectedColor) acc.UpdateAcsColor02(savedColor.Value);
+
+                        var color2 = chara.nowCoordinate.accessory.parts[slotNo].color[2];
+                        if(color2 == selectedColor) acc.UpdateAcsColor03(savedColor.Value);
+                    }
+
+                    if(moreAccObj != null)
+                    {
+                        var traverse = Traverse.Create(moreAccObj);
+                        var nowAccessories = traverse.Field("_charaMakerData").Field("nowAccessories").GetValue<List<ChaFileAccessory.PartsInfo>>();
+                        var charaMakerSlotDataList = traverse.Field("_additionalCharaMakerSlots").GetValue<IList>();
+                        foreach(var data in charaMakerSlotDataList)
+                        {
+                            var acc = Traverse.Create(data).Field("cvsAccessory").GetValue<CvsAccessory>();
+                            int slotNo = (int)acc.slotNo - 20;
+
+                            var color0 = nowAccessories[slotNo].color[0];
+                            if(color0 == selectedColor) acc.UpdateAcsColor01(savedColor.Value);
+
+                            var color1 = nowAccessories[slotNo].color[1];
+                            if(color1 == selectedColor) acc.UpdateAcsColor02(savedColor.Value);
+
+                            var color2 = nowAccessories[slotNo].color[2];
+                            if(color2 == selectedColor) acc.UpdateAcsColor03(savedColor.Value);
+                        }
+                    }
+
+                    Log(LogLevel.Message, "Color applied to accessories.");
+                    return; 
                 }
+            }
 
-                Log(LogLevel.Message, "Color applied to accessories.");
-            }
-            else
-            {
-                Log(LogLevel.Message, "Save a color before applying it to the accessories.");
-            }
+            Log(LogLevel.Message, "Save a color before applying it to the accessories.");
         }
 
         Color? GetSelectedColor()
         {
-            try
+            int selectIndex = accMenu.GetSelectIndex();
+
+            if(selectIndex == -1)
             {
-                var chara = CustomBase.Instance.chaCtrl;
-                var accMenu = FindObjectOfType<CustomAcsChangeSlot>();
-                var cvsAccessory = Traverse.Create(accMenu).Field("cvsAccessory").GetValue<CvsAccessory[]>();
-                int selectIndex = accMenu.GetSelectIndex();
+                if(moreAccObj != null)
+                {
+                    var traverse = Traverse.Create(moreAccObj);
+                    var nowAccessories = traverse.Field("_charaMakerData").Field("nowAccessories").GetValue<List<ChaFileAccessory.PartsInfo>>();
+                    int moreaccSelectedIndex = traverse.Method("GetSelectedMakerIndex").GetValue<int>();
+                    var charaMakerSlotDataList = traverse.Field("_additionalCharaMakerSlots").GetValue<IList>();
+                    var selected = charaMakerSlotDataList[moreaccSelectedIndex - 20];
+                    var cvsAccessory = Traverse.Create(selected).Field("cvsAccessory").GetValue<CvsAccessory>();
+                    var cvsColor = Traverse.Create(cvsAccessory).Field("cvsColor").GetValue<CvsColor>();
+
+                    if(cvsColor.connectColorKind != CvsColor.ConnectColorKind.None)
+                    {
+                        int colorIndex = ((int)cvsColor.connectColorKind - (int)CvsColor.ConnectColorKind.AcsSlot01_01) % 4;
+                        return nowAccessories[(int)cvsAccessory.slotNo - 20].color[colorIndex];
+                    }
+                }
+            }
+            else if(selectIndex >= 0)
+            {
                 var selected = cvsAccessory[selectIndex];
                 var cvsColor = Traverse.Create(selected).Field("cvsColor").GetValue<CvsColor>();
 
                 if(cvsColor.connectColorKind != CvsColor.ConnectColorKind.None)
                 {
-                    string kind = cvsColor.connectColorKind.ToString();
-                    int selectedColorIndex = int.Parse(kind.Substring(kind.Length - 1)) - 1;
-                    return chara.nowCoordinate.accessory.parts[(int)selected.slotNo].color[selectedColorIndex];
+                    int colorIndex = ((int)cvsColor.connectColorKind - (int)CvsColor.ConnectColorKind.AcsSlot01_01) % 4;
+                    return chara.nowCoordinate.accessory.parts[(int)selected.slotNo].color[colorIndex];
                 }
-            }
-            catch(Exception)
-            {
-                
             }
 
             return null;

5949b3c0edadb5042eabf4537394786f3e6a16bf
Keelhauled <keelhauledhs@gmail.com>
2019-02-06 16:43:55 +0200

    Shared HarmonyExtension project

---
+++ HarmonyExtension/HarmonyExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Harmony
+{
+    static class HarmonyExtension
+    {
+        public static void UnpatchAll(this HarmonyInstance harmony, Type type)
+        {
+            foreach(var met in type.GetMethods().Where(x => x.IsStatic && x.IsPublic))
+            foreach(var attr in met.GetCustomAttributes(false))
+                if(attr is HarmonyPatch hp)
+                {
+                    var original = AccessTools.Method(hp.info.originalType, hp.info.methodName, hp.info.parameter);
+                    harmony.RemovePatch(original, met);
+                }
+        }
+    }
+}

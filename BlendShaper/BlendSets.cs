babef1ec835261d20c1b7aa9651fe18b44813e81
Keelhauled <keelhauledhs@gmail.com>
2019-02-07 00:15:31 +0200

    BlendShaper - Use predetermined list for UI

---
+++ BlendShaper/BlendSets.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using ParadoxNotion.Serialization;
+using Studio;
+using UnityEngine;
+
+namespace BlendShaper
+{
+    class BlendSets
+    {
+        const string resourceFile = "BlendSets.json";
+
+        public static BlendSets LoadBlendSetData()
+        {
+            string resourceName = $"{nameof(BlendShaper)}.{resourceFile}";
+            using(var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+            {
+                using(var reader = new StreamReader(stream))
+                {
+                    string json = reader.ReadToEnd();
+                    return JSONSerializer.Deserialize<BlendSets>(json);
+                }
+            }
+        }
+
+        public List<Category> Categories;
+
+        public class Category
+        {
+            public string Name;
+            public List<Set> Sets;
+        }
+
+        public class Set
+        {
+            public void ChangeValue(float value)
+            {
+                Enabled = true;
+                Value = value;
+            }
+
+            public void CreateAction(OCIChar chara)
+            {
+                var skinnedMeshRenderers = chara.charInfo.animBody.GetComponentsInChildren<SkinnedMeshRenderer>(true).Where(x => x.sharedMesh && x.sharedMesh.blendShapeCount > 0);
+
+                var firstShape = Shapes.First();
+                Value = skinnedMeshRenderers.FirstOrDefault((x) => x.name == firstShape.Renderer).GetBlendShapeWeight(firstShape.Index);
+
+                Action = () =>
+                {
+                    foreach(var renderer in skinnedMeshRenderers)
+                    {
+                        foreach(var shape in Shapes)
+                        {
+                            if(renderer.name == shape.Renderer)
+                                renderer.SetBlendShapeWeight(shape.Index, Value);
+                        }
+                    }
+                };
+            }
+
+            public bool Enabled = false;
+            public float Value;
+            public Action Action;
+
+            public string Name;
+            public List<Shape> Shapes;
+
+            public class Shape
+            {
+                public string Renderer;
+                public int Index;
+            }
+        }
+    }
+}
--- BlendShaper/BlendShaper.cs
+++ BlendShaper/BlendShaper.cs
@@ -9,7 +9,20 @@ using UILib;
 using UnityEngine;
 using UnityEngine.UI;
 using Studio;
-//using Harmony;
+
+//cf_O_canine
+//cf_O_face
+//cf_O_mayuge
+//cf_O_noseline
+//cf_O_tooth
+//cf_O_eyeline
+//cf_O_eyeline_low
+//cf_O_namida_L
+//cf_O_namida_M
+//cf_O_namida_S
+//cf_Ohitomi_L
+//cf_Ohitomi_R
+//o_tang
 
 namespace BlendShaper
 {
@@ -18,26 +31,22 @@ namespace BlendShaper
     class BlendShaper : BaseUnityPlugin
     {
         public const string Version = "1.0.0";
-        //HarmonyInstance harmony;
 
         float UIScale = 1.0f;
         float elementSize = 20f;
 
         Canvas UISystem;
-        ScrollRect rendererList;
-        ScrollRect shapeList;
-        static List<Action> changes = new List<Action>();
+        ScrollRect categoryList;
+        ScrollRect blendSetList;
+        BlendSets blendSets = BlendSets.LoadBlendSetData();
 
-        void Awake()
+        void Start()
         {
-            //harmony = HarmonyInstance.Create("keelhauled.blendshaper.harmony");
-            //harmony.PatchAll(GetType());
-
             UIUtility.Init(nameof(BlendShaper));
 
             var panel = CreateUIBase(600f, 400f);
-            rendererList = CreateList(panel.transform, 0f, 0f, 0.3f, 1f);
-            shapeList = CreateList(panel.transform, 0.3f, 0f, 1f, 1f);
+            categoryList = CreateList(panel.transform, 0f, 0f, 0.3f, 1f);
+            blendSetList = CreateList(panel.transform, 0.3f, 0f, 1f, 1f);
 
             if(Studio.Studio.Instance.treeNodeCtrl.selectNode)
                 UpdateUI(Studio.Studio.Instance.treeNodeCtrl.selectNode);
@@ -51,75 +60,75 @@ namespace BlendShaper
             Destroy(UISystem);
             Studio.Studio.Instance.treeNodeCtrl.onSelect -= UpdateUI;
             Studio.Studio.Instance.treeNodeCtrl.onDelete -= ClearUI;
-            //harmony.UnpatchAll(GetType());
         }
 
         void LateUpdate()
         {
-            if(changes.Count > 0)
+            foreach(var category in blendSets.Categories)
             {
-                foreach(var change in changes)
-                    change();
+                foreach(var set in category.Sets)
+                {
+                    if(set.Enabled)
+                        set.Action();
+                }
             }
         }
 
         void UpdateUI(TreeNodeObject node)
         {
-            ClearUI(Studio.Studio.Instance.treeNodeCtrl.selectNode);
+            ClearUI(node);
 
             if(Studio.Studio.Instance.dicInfo.TryGetValue(node, out var objectCtrlInfo))
             {
                 if(objectCtrlInfo is OCIChar chara)
                 {
-                    var skinnedMeshRenderers = chara.charInfo.animBody.GetComponentsInChildren<SkinnedMeshRenderer>(true).Where(x => x.sharedMesh.blendShapeCount > 0);
-                    foreach(var renderer in skinnedMeshRenderers)
+                    foreach(var category in blendSets.Categories)
                     {
-                        var button = UIUtility.CreateButton("Button", rendererList.content.transform, renderer.name);
+                        var button = UIUtility.CreateButton("Button", categoryList.content.transform, category.Name);
                         button.gameObject.AddComponent<LayoutElement>().preferredHeight = elementSize;
-                        button.onClick.AddListener(() => UpdateBlend(renderer));
+                        button.onClick.AddListener(() => UpdateBlend(category.Sets, chara));
                     }
                 }
             }
         }
 
-        void UpdateBlend(SkinnedMeshRenderer renderer)
+        void UpdateBlend(List<BlendSets.Set> sets, OCIChar chara)
         {
-            foreach(var button in shapeList.content.GetComponentsInChildren<Image>())
+            foreach(var button in blendSetList.content.GetComponentsInChildren<Image>())
             {
                 button.gameObject.SetActive(false);
                 Destroy(button);
             }
 
-            for(int i = 0; i < renderer.sharedMesh.blendShapeCount; i++)
+            foreach(var set in sets)
             {
-                var index = i;
+                set.CreateAction(chara);
 
-                var panel = UIUtility.CreatePanel("ShapePanel", shapeList.content.transform);
+                var panel = UIUtility.CreatePanel("ShapePanel", blendSetList.content.transform);
                 panel.gameObject.AddComponent<LayoutElement>().preferredHeight = elementSize;
                 panel.color = new Color(1f, 1f, 1f, 1f);
-
-                var shapeName = renderer.sharedMesh.GetBlendShapeName(index);
-                var text = UIUtility.CreateText("ShapeName", panel.transform, shapeName);
+                
+                var text = UIUtility.CreateText("ShapeName", panel.transform, set.Name);
                 text.transform.SetRect(0f, 0f, 0.3f, 1f);
                 text.color = Color.black;
 
                 var slider = UIUtility.CreateSlider("ShapeSlider", panel.transform);
                 slider.transform.SetRect(0.3f, 0f, 1f, 1f);
                 slider.maxValue = 100f; slider.minValue = 0f;
-                slider.value = renderer.GetBlendShapeWeight(index);
-                slider.onValueChanged.AddListener((value) => changes.Add(() => renderer.SetBlendShapeWeight(index, value)));
+                slider.value = set.Value;
+                slider.onValueChanged.AddListener((value) => set.ChangeValue(value));
             }
         }
 
         void ClearUI(TreeNodeObject node)
         {
-            foreach(var button in rendererList.content.GetComponentsInChildren<Button>())
+            foreach(var button in categoryList.content.GetComponentsInChildren<Button>())
             {
                 button.gameObject.SetActive(false);
                 Destroy(button);
             }
 
-            foreach(var button in shapeList.content.GetComponentsInChildren<Image>())
+            foreach(var button in blendSetList.content.GetComponentsInChildren<Image>())
             {
                 button.gameObject.SetActive(false);
                 Destroy(button);
@@ -136,7 +145,7 @@ namespace BlendShaper
             UISystem.transform.SetParent(gameObject.transform);
 
             var bg = UIUtility.CreatePanel("Background", UISystem.transform);
-            bg.transform.SetRect(0.5f, 0.5f, 0.5f, 0.5f, -width / 2f, -height / 2f, width / 2f, height / 2f);
+            bg.transform.SetRect(0.5f, 0.3f, 0.5f, 0.3f, -width / 2f, -height / 2f, width / 2f, height / 2f);
             bg.color = new Color(bg.color.r, bg.color.g, bg.color.b, 1f);
 
             var drag = UIUtility.CreatePanel("Drag", bg.transform);

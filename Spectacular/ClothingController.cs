b6b8b5fcb287d8e0e382259cb593cf552495e6bd
Keelhauled <keelhauledhs@gmail.com>
2019-02-25 23:07:30 +0200

    Clothing specularity plugin

---
+++ Spectacular/ClothingController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using BepInEx.Logging;
+using Logger = BepInEx.Logger;
+using UnityEngine;
+using Harmony;
+
+namespace Spectacular
+{
+    static class ClothingController
+    {
+        static ChaClothesComponent currentClothesComponent;
+        static int _SpecularColor = Shader.PropertyToID("_SpecularColor");
+        static int _SpecularPower = Shader.PropertyToID("_SpecularPower");
+
+        public static void ChangeSpecularity(float val)
+        {
+            if(currentClothesComponent)
+            {
+                var rendererArrs = GetRendererArrays(currentClothesComponent);
+                var applicableRenderers = GetApplicableRenderers(rendererArrs).ToList();
+
+                foreach(var renderer in applicableRenderers)
+                {
+                    if(renderer.material.HasProperty(_SpecularPower))
+                        renderer.material.SetFloat(_SpecularPower, val);
+                }
+            }
+            else
+            {
+                Logger.Log(LogLevel.Info, "currentClothesComponent == null");
+            }
+        }
+
+        public static void ChangeSpecColor(Color val)
+        {
+            if(currentClothesComponent)
+            {
+                var rendererArrs = GetRendererArrays(currentClothesComponent);
+                var applicableRenderers = GetApplicableRenderers(rendererArrs).ToList();
+
+                foreach(var renderer in applicableRenderers)
+                {
+                    if(renderer.material.HasProperty(_SpecularColor))
+                        renderer.material.SetColor(_SpecularColor, val);
+                }
+            }
+            else
+            {
+                Logger.Log(LogLevel.Info, "currentClothesComponent == null");
+            }
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(ChaControl), nameof(ChaControl.ChangeCustomClothes))]
+        public static void ChangeCustomClothesPostHook(ChaControl __instance, ref bool main, ref int kind)
+        {
+            var clothesCtrl = GetCustomClothesComponent(__instance, main, kind);
+            if(!clothesCtrl)
+            {
+                Logger.Log(LogLevel.Info, "No clothes component found");
+                return;
+            }
+
+            currentClothesComponent = clothesCtrl;
+        }
+
+        static ChaClothesComponent GetCustomClothesComponent(ChaControl chaControl, bool main, int kind)
+        {
+            // for top clothes it fires once at start with first bool true (main load), then for each subpart with bool false
+            // if true, objClothes are used, if false objParts                
+            return main ? chaControl.GetCustomClothesComponent(kind) : chaControl.GetCustomClothesSubComponent(kind);
+        }
+
+        static Renderer[][] GetRendererArrays(ChaClothesComponent clothesCtrl)
+        {
+            return new[] { clothesCtrl.rendNormal01, clothesCtrl.rendNormal02, clothesCtrl.rendAlpha01, clothesCtrl.rendAlpha02 };
+        }
+
+        static IEnumerable<Renderer> GetApplicableRenderers(Renderer[][] rendererArrs)
+        {
+            for(var i = 0; i < rendererArrs.Length; i += 2)
+            {
+                var renderers = rendererArrs[i];
+                var renderer1 = renderers?.ElementAtOrDefault(0);
+                if(renderer1 != null)
+                {
+                    yield return renderer1;
+
+                    renderers = rendererArrs.ElementAtOrDefault(i + 1);
+                    var renderer2 = renderers?.ElementAtOrDefault(0);
+                    if(renderer2 != null)
+                        yield return renderer2;
+
+                    yield break;
+                }
+            }
+        }
+    }
+}
---
+++ Spectacular/Spectacular.cs
@@ -0,0 +1,54 @@
+﻿using BepInEx;
+using KKAPI.Maker;
+using KKAPI.Maker.UI;
+using UniRx;
+using Harmony;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Spectacular
+{
+    [BepInProcess("Koikatu")]
+    [BepInPlugin(GUID, "Spectacular", Version)]
+    public class Spectacular : BaseUnityPlugin
+    {
+        public const string GUID = "keelhauled.spectacular";
+        public const string Version = "1.0.0";
+
+        HarmonyInstance harmony;
+
+        void Start()
+        {
+            MakerAPI.RegisterCustomSubCategories += RegisterCustomSubCategories;
+            harmony = HarmonyInstance.Create("keelhauled.spectacular.harmony");
+            harmony.PatchAll(typeof(ClothingController));
+        }
+
+        void RegisterCustomSubCategories(object sender, RegisterSubCategoriesEvent e)
+        {
+            var categories = new List<MakerCategory>
+            {
+                MakerConstants.Clothes.Top,
+                MakerConstants.Clothes.Bottom,
+                MakerConstants.Clothes.Bra,
+                MakerConstants.Clothes.Panst,
+                MakerConstants.Clothes.Gloves,
+                MakerConstants.Clothes.Shorts,
+                MakerConstants.Clothes.InnerShoes,
+                MakerConstants.Clothes.OuterShoes,
+                MakerConstants.Clothes.Socks,
+            };
+
+            foreach(var category in categories)
+            {
+                var slider = new MakerSlider(category, "Specularity", 0f, 1f, 0f, this);
+                slider.ValueChanged.Subscribe((val) => ClothingController.ChangeSpecularity(val));
+                e.AddControl(slider);
+
+                var color = new MakerColor("Spec color", false, category, Color.white, this);
+                color.ValueChanged.Subscribe((val) => ClothingController.ChangeSpecColor(val));
+                e.AddControl(color);
+            }
+        }
+    }
+}
